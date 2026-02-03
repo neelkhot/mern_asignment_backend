5bbea35ad11ab1d0325132eb4300e89cc4a8c1e7
Neel khot <neelkhot1008@gmail.com>
2026-02-04 01:52:28 +0530

    2nd commit

--- api/index.js
+++ api/index.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("../routes/userRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);  
-
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/users", userRoutes);
 
-
+module.exports = app;
